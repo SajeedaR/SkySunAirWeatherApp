d1f766f74601090683af7b09b249f6a74b4d05d9
SajeedaR <sajeeda1@gmail.com>
2024-12-09 11:32:01 +0200

    Converted demi to live response data and targeted css to accomodate formatting

--- src/index.js
+++ src/index.js
@@ -54,6 +54,11 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 
 function getForecast(city) {
   let apiKey = "bb3a028tf97bc5df05b8co2a43154245";
@@ -64,22 +69,29 @@ function getForecast(city) {
 
 function displayForecast(response) {
   console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅️</div>
+            <div class="weather-forecast-date"> ${formatDay(day.time)}</div>
+             <img src= "${
+               day.condition.icon_url
+             }" class = "weather-forecast-icon" />
+          </div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>19°</strong> 15°
+                <strong>${Math.round(
+                  day.temperature.maximum
+                )}°c</strong> ${Math.round(day.temperature.minimum)}°c
               </div>
             </div>
           </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
